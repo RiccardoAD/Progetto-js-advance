b82773553107a53f31088b42340aaa09e1abccfb
RiccardoAD <rick_95@libero.it>
2024-09-25 23:00:03 +0200

    21

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: "production",
     entry: "./src/js/index.js",
     output: {
-      filename: "main.[contenthash].js",
+      filename: "main.[hash].js",
       path: path.resolve(__dirname, "dist"),
       clean: true,
     },
@@ -23,5 +23,5 @@ module.exports = {
         template: "./src/index.html",
       }),
     ],
-    devtool: 'source-map', 
+    
   };
